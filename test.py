a9ded0ece7728f8918b496f3a5793748bbd888a3
adhayaimakmak <adha4728@gmail.com>
2026-03-30 22:02:53 +0700

    add new data and add new Feature in test.py

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import pandas as pd
-df = pd.read_csv('sleep_health_dataset.csv')
-print(df.head())
+
+class DataProcessor:
+    def __init__(self, file_path):
+        self.data = pd.read_csv(file_path)
+    def __str__(self):
+        return self.data.head().to_string()
+
+def main():
+    data = DataProcessor(r'sales.csv')
+    print(data)
+if __name__ == "__main__":
+    main()
